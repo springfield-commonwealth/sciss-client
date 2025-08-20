e5f2b6328a8b25a3b35aeb6088124902af319626
kqin-sca <kyle.qin@springfieldca.org>
2025-08-20 15:43:48 -0700

    fix: add _document.js to resolve Html import error during static generation

--- src/hooks/useNavigationState.js
+++ src/hooks/useNavigationState.js
@@ -131,7 +131,7 @@ const useNavigationState = (router) => {
     if (!router || !router.pathname) {
       return false;
     }
-    
+
     if (href === "/") {
       return router.pathname === "/";
     }

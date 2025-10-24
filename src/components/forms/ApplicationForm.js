38679009388f76f5e8be678032af77f3819095f7
Toula Elwell <tae9467@rit.edu>
2025-10-24 14:42:47 -0400

    Add detailed debugging for course and sports values

--- src/components/forms/ApplicationForm.js
+++ src/components/forms/ApplicationForm.js
@@ -137,6 +137,8 @@ const ApplicationForm = () => {
     console.log('Form submitted!', e);
     e.preventDefault();
     console.log('Form values:', formValues);
+    console.log('Course values:', formValues.course);
+    console.log('Sports values:', formValues.sports);
     console.log('Form errors:', errors);
     console.log('Form isValid:', isValid);
     Object.keys(formValues).forEach((field) => {
